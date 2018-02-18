08163b586b85f36d6691497b54cde0a9d609fca8
Elfert-19 <rcg1990@gmail.com>
2018-02-18 20:20:18 -0300

    Primera version de Game Manager(Solo controla que los jugadores esten vivos) y una escena placeholder para la victoria.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+    public string pantallaMenu;
+    public string pantallaVictoria;
+    public GameObject jugador1;
+    public GameObject jugador2;
+    public GameObject jugador3;
+    public GameObject jugador4;
+    GameObject[] jugadores;
+    public bool rondaEnCurso;
+    public string ganador;
+
+    void Awake(){
+		if(instance == null){
+			instance = this;
+		}else{
+			Destroy(gameObject);
+		}
+        jugadores = new GameObject[4];
+	}
+
+    void Update(){
+        if (rondaEnCurso == true){
+            ControlRonda();
+        }
+	}
+
+    public void CargarPantallaMenu(){
+        SceneManager.LoadScene(pantallaMenu);
+    }
+
+    public void CaragarPanatallaVitoria(){
+        SceneManager.LoadScene(pantallaVictoria);
+    }
+
+    public void EnsambladorRonda(){
+        if (jugador1 != null){
+            jugadores[1] = jugador1;
+        }
+        if (jugador2 != null){
+            jugadores[2] = jugador2;
+        }
+        if (jugador3 != null){
+            jugadores[3] = jugador3;
+        }
+        if (jugador4 != null){
+            jugadores[4] = jugador4;
+        }
+    }
+
+    public void ControlRonda(){
+        int vivos = 0;
+        for (int i = 0; i < jugadores.Length; i++){
+            if (jugadores[i].activeInHierarchy == true){
+                vivos += 1;
+            }
+        }
+        if(vivos == 1){
+            for (int i = 0; i < jugadores.Length; i++){
+                if(jugadores[i].activeSelf == true){
+                    ganador = jugadores[i].name;
+                }
+            }
+        }
+        if(vivos == 0){
+            CargarPantallaMenu();
+        }
+    }
+
+}
