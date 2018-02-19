6019d8863666412ef97cf143b591a8c388bbb3a3
Elfert-19 <rcg1990@gmail.com>
2018-02-18 23:08:21 -0300

    Un asignador de jugador y un scrip de vida, para integrarselo al prefab del jugador cuando lo creen.

---
+++ Assets/Scripts/Jugador/AsignadorJugador.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsignadorJugador : MonoBehaviour {
+    public bool jugador1;
+    public bool jugador2;
+    public bool jugador3;
+    public bool jugador4;
+
+    private void Awake(){
+        Asignador();
+    }
+
+    void Asignador(){
+        if(GameManager.instance.jugador1 == null){
+            GameManager.instance.jugador1 = gameObject;
+            jugador1 = true;
+        }else if(GameManager.instance.jugador2 == null){
+            GameManager.instance.jugador2 = gameObject;
+            jugador2 = true;
+        }
+        else if(GameManager.instance.jugador3 == null){
+            GameManager.instance.jugador3 = gameObject;
+            jugador3 = true;
+        }
+        else{
+            GameManager.instance.jugador4 = gameObject;
+            jugador4 = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Jugador/Vida.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vida : MonoBehaviour {
+    public int vida;
+
+    private void Update(){
+        ControlVida();
+    }
+
+    void ControlVida(){
+        if(vida <= 0){
+            Destroy(gameObject);
+        }
+    }
+
+    public void Damage(int hit){
+        vida -= hit;
+    }
+
+    public void Healer(int aCurar){
+        vida += aCurar;
+    }
+}
