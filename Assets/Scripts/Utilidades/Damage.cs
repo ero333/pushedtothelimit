d5a77818523efa6357dd6cf1cf3cde62060cab83
Elfert-19 <rcg1990@gmail.com>
2018-02-19 18:59:29 -0300

    Scrip Dañador, configure un par de layers y la deteccion entre ellas y configure todas las imagenes subidas a Sprites.

---
+++ Assets/Scripts/Utilidades/Damage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+    public int dano;
+
+    private void OnTriggerEnter(Collider hit){
+        if(hit.GetComponent<Vida>() != null){
+            Vida objetivo = hit.GetComponent<Vida>();
+            objetivo.Damage(dano);
+        }
+    }
+}
