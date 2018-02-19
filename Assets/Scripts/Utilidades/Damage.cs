ca8352db9d3af349f70c1f942ac11838eb369a39
Elfert-19 <rcg1990@gmail.com>
2018-02-19 20:02:40 -0300

    Fix del Scrip

--- Assets/Scripts/Utilidades/Damage.cs
+++ Assets/Scripts/Utilidades/Damage.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Damage : MonoBehaviour {
     public int dano;
 
-    private void OnTriggerEnter(Collider hit){
-        if(hit.GetComponent<Vida>() != null){
+    private void OnTriggerEnter2D(Collider2D hit){
+        if (hit.GetComponent<Vida>() != null)
+        {
             Vida objetivo = hit.GetComponent<Vida>();
             objetivo.Damage(dano);
         }
