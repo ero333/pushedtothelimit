47c5d0692fb627c003b431be5abae0254f31bb34
PatoParodi <36526703+PatoParodi@users.noreply.github.com>
2018-02-19 00:03:48 -0300

    Manager de Planetas
    Agrego los script para los planetas

---
+++ Assets/Scripts/Managers/PlanetaManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetaManager : MonoBehaviour {
+
+	public float velocidadRotacion;
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Rotate (Vector3.forward * Time.deltaTime * velocidadRotacion);
+
+	}
+}
