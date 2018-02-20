32094d9ad0630663e6c85da56af86edf5a225f4c
MicaelaAr95 <mica.gallinger@gmail.com>
2018-02-20 16:53:54 -0300

    Script para diferenciar al jugador por su color

---
+++ Assets/Scripts/Jugador/ColorDiferencial.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorDiferencial : MonoBehaviour {
+
+	//Este scrip le da un color al jugador ni bien empieza el nivel dependiendo de su nombre
+
+	public Color[] variables;
+	SpriteRenderer colorDelPlayer;
+
+	// Use this for initialization
+	void Start () {
+
+		colorDelPlayer = gameObject.GetComponent <SpriteRenderer>();
+
+		switch (gameObject.name){
+		case "Jugador1":
+			colorDelPlayer.color = variables [0];
+			break;
+		case "Jugador2":
+			colorDelPlayer.color = variables [1];
+			break;
+		case "Jugador3":
+			colorDelPlayer.color = variables [2];
+			break;
+		case "Jugador4":
+			colorDelPlayer.color = variables [3];
+			break;
+
+		}
+	}
+}
