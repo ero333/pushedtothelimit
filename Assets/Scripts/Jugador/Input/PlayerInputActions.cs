9aba892cb85babff044ef6f5238d5229ec6bfe9c
Luciano McBride <kenny.ramone91@gmail.com>
2018-02-24 10:05:40 -0300

    Agregar scripts PlayerInputActions y PlayerInputManager

---
+++ Assets/Scripts/Jugador/Input/PlayerInputActions.cs
@@ -0,0 +1,78 @@
+﻿using InControl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Clase para almacenar el set de acciones de input que va a poder realizar el jugador. Es parte del plugin InControl.
+/// Para más, ver http://www.gallantgames.com/pages/incontrol-binding-actions-to-controls
+/// </summary>
+
+public class PlayerInputActions : PlayerActionSet
+{
+    public PlayerAction MoveLeft { get; private set; }
+    public PlayerAction MoveRight { get; private set; }
+    public PlayerOneAxisAction Move { get; private set; }
+
+    public PlayerAction Jump { get; private set; }
+    
+    public PlayerAction IgniteJetpack { get; private set; }
+    public PlayerAction RotateLeft { get; private set; }
+    public PlayerAction RotateRight { get; private set; }
+    public PlayerOneAxisAction RotateJetpack { get; private set; }
+
+    public PlayerAction Shoot { get; private set; }
+
+    public PlayerAction AimUp { get; private set; }
+    public PlayerAction AimDown { get; private set; }
+    public PlayerAction AimLeft { get; private set; }
+    public PlayerAction AimRight { get; private set; }
+    public PlayerTwoAxisAction Aim { get; private set; }
+
+    public PlayerAction RotateAimLeft { get; private set; }
+    public PlayerAction RotateAimRight { get; private set; }
+
+
+    public PlayerInputActions()
+    {
+        MoveLeft = CreatePlayerAction("Move Left");
+        MoveRight = CreatePlayerAction("Move Right");
+        Move = CreateOneAxisPlayerAction(MoveLeft, MoveRight);
+
+        Jump = CreatePlayerAction("Jump");
+
+        IgniteJetpack = CreatePlayerAction("Thrust");
+        RotateLeft = CreatePlayerAction("Rotate Left");
+        RotateRight = CreatePlayerAction("Rotate Right");
+        RotateJetpack = CreateOneAxisPlayerAction(RotateLeft, RotateRight);
+
+        Shoot = CreatePlayerAction("Shoot");
+
+        AimUp = CreatePlayerAction("Aim Up");
+        AimDown = CreatePlayerAction("Aim Down");
+        AimLeft = CreatePlayerAction("Aim Left");
+        AimRight = CreatePlayerAction("Aim Right");
+        Aim = CreateTwoAxisPlayerAction(AimLeft, AimRight, AimDown, AimUp);
+
+        SetupDefaultBindings();
+    }
+
+    private void SetupDefaultBindings()
+    {
+        MoveLeft.AddDefaultBinding(InputControlType.LeftStickLeft);
+        MoveRight.AddDefaultBinding(InputControlType.LeftStickRight);
+
+        Jump.AddDefaultBinding(InputControlType.Action1);
+
+        IgniteJetpack.AddDefaultBinding(InputControlType.LeftStickUp);
+        RotateLeft.AddDefaultBinding(InputControlType.LeftStickLeft);
+        RotateRight.AddDefaultBinding(InputControlType.LeftStickRight);
+
+        Shoot.AddDefaultBinding(InputControlType.Action2);
+
+        AimUp.AddDefaultBinding(InputControlType.RightStickUp);
+        AimDown.AddDefaultBinding(InputControlType.RightStickDown);
+        AimLeft.AddDefaultBinding(InputControlType.RightStickLeft);
+        AimRight.AddDefaultBinding(InputControlType.RightStickRight);
+    }
+}
---
+++ Assets/Scripts/Jugador/Input/PlayerInputManager.cs
@@ -0,0 +1,60 @@
+﻿using InControl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputManager
+{
+    private PlayerController _player;
+    private InputDevice _device;
+
+    private PlayerInputActions _actions;
+
+    #region Input Values
+    public float MovementAxisValue { get { return _actions.Move; } }
+    public bool JumpWasJustPressed { get { return _actions.Jump.IsPressed && _actions.Jump.HasChanged; } }
+
+    public bool ShootIsPressed { get { return _actions.Jump.IsPressed; } }
+    public bool ShootWasJustPressed { get { return _actions.Jump.IsPressed && _actions.Jump.HasChanged; } }
+
+    public Vector2 AimAxisValue { get { return _actions.Aim; } }
+
+    public bool JetpackThurstIsPressed { get { return _actions.IgniteJetpack.IsPressed; } }
+    public float JetpackRotationAxisValue { get { return _actions.RotateJetpack; } }
+
+    #endregion
+
+    public PlayerInputManager(PlayerController player)
+    {
+        _player = player;
+
+        _actions = new PlayerInputActions();
+
+        //TODO: ¿Es esta la forma en la que queremos implementar la asignación de controles?
+
+        foreach (InputDevice device in InputManager.Devices)
+        {
+            if (device.IsAttached)
+            {
+                _device = device;
+            }
+
+        }
+
+        _actions.Device = _device;
+    }
+
+    public void Vibrate(float intensity, float duration)
+    {
+        if (duration <= 0 || intensity <= 0) return;
+
+        _player.StartCoroutine(MakeGamepadVibrate(intensity, duration));
+    }
+
+    private IEnumerator MakeGamepadVibrate(float intensity, float duration)
+    {
+        _device.Vibrate(intensity);
+        yield return new WaitForSeconds(duration);
+        _device.StopVibration();
+    }
+}
