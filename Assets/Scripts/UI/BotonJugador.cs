1fcc6647b51aabb99252bb8c6a114c03a596dcc0
Elfert-19 <rcg1990@gmail.com>
2018-02-26 22:43:51 -0300

    Nueva version de PantallaJugadores (Mas clara y facil de acceder)

---
+++ Assets/Scripts/UI/BotonJugador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonJugador : MonoBehaviour {
+    public int jugadoresAIstanciar;
+    public GameObject jugador;
+    public string escenaAJugar;
+
+    public void InstanciadorJugadores(){
+        for (int i = 0; i < jugadoresAIstanciar; i++)
+        {
+            Instantiate(jugador);
+        }
+        SceneManager.LoadScene(escenaAJugar);
+    }
+}
