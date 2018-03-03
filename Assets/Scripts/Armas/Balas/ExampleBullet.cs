f26d4832495d062e41b4b112f2bf08257019e7b3
Luciano McBride <kenny.ramone91@gmail.com>
2018-03-03 14:35:56 -0300

    Herramientas de debug y ejemplos para las armas

---
+++ Assets/Scripts/Armas/Balas/ExampleBullet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ExampleBullet : BaseBullet
+{
+    private Weapon _weapon;
+
+    public override void OnShot()
+    {
+        Debug.Log("ExampleBullet disparada", gameObject);
+    }
+
+    protected override void ApplyEffectOnPlayer(GameObject player)
+    {
+        Destroy(player);
+    }
+}
---
+++ Assets/Scripts/Armas/Editor/WeaponEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Weapon))]
+public class WeaponEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Fire Bullet"))
+        {
+            ((Weapon)target).Shoot();
+        }
+    }
+}
