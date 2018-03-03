8a6de7647917b23230391f64072d057a8a80f74a
Luciano McBride <kenny.ramone91@gmail.com>
2018-03-03 13:43:38 -0300

    Clases base para las armas y las balas

---
+++ Assets/Scripts/Armas/Balas/BaseBullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public abstract class BaseBullet : MonoBehaviour
+{
+    /*
+     *  [X] Sprite
+     *  [X] Algo que pasa cuando se dispara
+     *  [X] Algo que pasa cuando colisiona con el personaje  
+     *  [X] Algo que pasa cuanodo colisiona contra un planeta
+     *  [ ] Partículas cuando se destruyen?
+     */
+
+    public abstract void OnShot();
+
+    protected virtual void ApplyEffectOnPlayer(GameObject player) { }
+    protected virtual void ApplyEffectOnPlanet(GameObject planet) { }
+
+    private void OnCollisionWithPlayer(GameObject player)
+    {
+        ApplyEffectOnPlayer(player);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionWithPlanet(GameObject planet)
+    {
+        ApplyEffectOnPlanet(planet);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            OnCollisionWithPlayer(collision.gameObject);
+            return;
+        }
+
+        if (collision.gameObject.CompareTag("Planet"))
+        {
+            OnCollisionWithPlanet(collision.gameObject);
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/Armas/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]
+    private BaseBullet _bullet;
+
+    private Transform _bulletSpawnPoint;
+    private ParticleSystem _particles;
+
+    private void Awake()
+    {
+        _bulletSpawnPoint = transform.Find("BulletSpawnPoint");
+        Debug.Assert(_bulletSpawnPoint != null, "Weapon prefab missing BulletSpawnPoint child.", gameObject);
+
+        _particles = transform.Find("Partices").GetComponent<ParticleSystem>();
+        Debug.Assert(_particles != null, "Weapon prefab missing Particles child.", gameObject);
+    }
+
+    public void Shoot()
+    {
+        _particles.Play();
+        BaseBullet newBullet = Instantiate(_bullet, _bulletSpawnPoint.position, Quaternion.identity, null);
+        newBullet.transform.right = _bulletSpawnPoint.transform.right;
+
+        newBullet.OnShot();
+    }
+}
