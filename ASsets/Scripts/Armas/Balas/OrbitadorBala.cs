1f5c578a0262b1a5df737d3de88a6d38b0354d5a
Elfert-19 <rcg1990@gmail.com>
2018-03-03 17:02:40 -0300

    Script para orbita de las balas y un manager para el spawn de intems en los planetas

---
+++ ASsets/Scripts/Armas/Balas/OrbitadorBala.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitadorBala : MonoBehaviour {
+
+    Rigidbody2D rbBala;
+    PlanetAttractor planeta;
+
+    private void Awake(){
+        rbBala = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate(){
+        if (planeta != null){
+            planeta.Attract(rbBala);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D hit){
+       
+        if (hit.CompareTag("Planet")){
+            planeta = hit.GetComponent<PlanetAttractor>();
+        }
+    }
+}
---
+++ ASsets/Scripts/Managers/SpawnItems.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItems : MonoBehaviour {
+
+    public List<GameObject> planetas;
+    public List<GameObject> elementoASpawnear;
+    int planetaElegido;
+    int poloelegido;
+    int itemElegido;
+    public float tiempoDeSpawn;
+
+    private void Awake(){
+        InvokeRepeating("LocacionElegida", tiempoDeSpawn,tiempoDeSpawn);
+    }
+
+    void LocacionElegida(){
+        planetaElegido = Random.Range(0, planetas.Count);
+        poloelegido = Random.Range(0, 3);
+        itemElegido = Random.Range(0, elementoASpawnear.Count);
+        InstanciarItem();
+    }
+
+    void InstanciarItem(){
+        ListaPoscionItem polo = planetas[planetaElegido].GetComponentInChildren<ListaPoscionItem>();
+        GameObject item = Instantiate(elementoASpawnear[itemElegido],polo.posiciones[poloelegido]);
+        item.transform.position = polo.posiciones[poloelegido].position;
+        item.transform.rotation = polo.posiciones[poloelegido].rotation;
+
+    }
+}
