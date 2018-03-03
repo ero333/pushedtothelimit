8d9aa3a35721dd7c0dfbe0394685964b9924fbb1
Luciano McBride <kenny.ramone91@gmail.com>
2018-03-03 14:34:13 -0300

    Agregar munición a las armas

--- Assets/Scripts/Armas/Weapon.cs
+++ Assets/Scripts/Armas/Weapon.cs
@@ -10,21 +10,43 @@ public class Weapon : MonoBehaviour
     private Transform _bulletSpawnPoint;
     private ParticleSystem _particles;
 
+    public bool IsEmpty
+    {
+        get
+        {
+            return _ammo != -1 && _ammo <= 0;
+        }
+    }
+
+    [SerializeField]
+    // "Cantidad de balas que va a tener el arma. Usar -1 si el arma no se gasta nunca."
+    [Tooltip("Cantidad de balas que va a tener el arma. Usar -1 si el arma no se gasta nunca.")]
+    private int _ammo;
+
     private void Awake()
     {
         _bulletSpawnPoint = transform.Find("BulletSpawnPoint");
         Debug.Assert(_bulletSpawnPoint != null, "Weapon prefab missing BulletSpawnPoint child.", gameObject);
 
-        _particles = transform.Find("Partices").GetComponent<ParticleSystem>();
+        _particles = transform.Find("Particles").GetComponent<ParticleSystem>();
         Debug.Assert(_particles != null, "Weapon prefab missing Particles child.", gameObject);
     }
 
     public void Shoot()
     {
+        if (_ammo == 0)
+        {
+            Debug.LogError("Trying to fire a weapon with no ammo. Should have switched to the default weapon!", gameObject);
+            return;
+        }
+
         _particles.Play();
         BaseBullet newBullet = Instantiate(_bullet, _bulletSpawnPoint.position, Quaternion.identity, null);
         newBullet.transform.right = _bulletSpawnPoint.transform.right;
 
         newBullet.OnShot();
+
+        if (_ammo != -1)
+            _ammo--;
     }
 }
