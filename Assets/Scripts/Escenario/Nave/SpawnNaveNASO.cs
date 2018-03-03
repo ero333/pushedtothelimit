b5fd0c8f3720863f2d8144ec991bf99c65d50a9f
norbyariel02 <norbertowinkler@gmail.com>
2018-03-02 21:57:55 -0300

    Reparando mas errores git LPMQTP

---
+++ Assets/Scripts/Escenario/Nave/SpawnNaveNASO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNaveNASO : MonoBehaviour {
+
+	public GameObject nave;
+
+
+	public void NaveAlRescate(){
+		Instantiate (nave, transform.position, transform.rotation);
+	}
+}
