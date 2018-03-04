e71aa53cb9e8c08dfc96ad9e668547ff37467534
Berdyx <berdyx@gmail.com>
2018-03-04 20:52:10 -0300

    Version inicial de pusher gun
    Version inicial de pusher gun, trabajando en fine tunning

---
+++ ASsets/Scripts/Armas/Balas/PusherBullet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PusherBullet : BaseBullet
+{
+    private Weapon _weapon;
+	private float pushPower = 20000.0f;
+	private float bulletSpeed = 10.0f;
+	private Rigidbody2D rbPlayer;
+	private Rigidbody2D rbBullet;
+	private Vector2 pushVelocidad;
+	private Vector2 pushFuerza;
+
+    public override void OnShot()
+    {
+		Debug.Log("ExampleBullet disparada", gameObject);
+		rbBullet = GetComponent<Rigidbody2D>();
+		//rbBullet.isKinematic = false;
+		//rbBullet.AddForce (transform.right * bulletSpeed);
+		rbBullet.velocity = transform.right * bulletSpeed;
+		Debug.Log ("La bala se mueve", gameObject);
+		Destroy (this.gameObject, 1);
+    }
+
+    protected override void ApplyEffectOnPlayer(GameObject player)
+    {
+		rbPlayer = player.GetComponent<Rigidbody2D> ();
+		rbPlayer.AddForce (rbPlayer.transform.right * pushPower);
+
+    }
+}
---
+++ ASsets/Scripts/Armas/WeaponController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+	//public GameObject _defaultWeapon;
+	//public GameObject _alternatWeapon;
+	private Weapon _activeWeapon;
+	int playername;
+	private PlayerController playcontrol;
+	private bool shoot = false;
+	private PlayerInputManager inputControl;
+
+	void Start () {
+		_activeWeapon = gameObject.GetComponentInChildren<Weapon> ();
+		playcontrol = GetComponent<PlayerController> ();
+		inputControl = playcontrol.GetInputManager;
+
+		if (playcontrol != null)
+			playername = playcontrol.PlayerName;
+
+	}
+
+	void Update(){
+		ShootWeapon (_activeWeapon);
+	}
+
+
+	public void ShootWeapon (Weapon newWeapon){
+
+		if (playername == 1) {
+			if (Input.GetKey (KeyCode.S)) {
+				Debug.Log ("Player 1 dispara");
+				shoot = true;
+			}
+			
+		}
+
+		if (playername == 2) {
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				Debug.Log ("Player 2 dispara");
+				shoot = true;
+			}
+		}
+
+		if (playername == 3) {
+			if (Input.GetKey (KeyCode.K))
+				shoot = true;
+		}
+
+		if (playername == 4) {
+			if (Input.GetKey (KeyCode.Keypad5))
+				shoot = true;
+		}
+
+		if (shoot) {
+			Debug.Log (shoot);
+			_activeWeapon.Shoot ();
+		}
+
+		shoot = false;
+	}
+}
